57f85d4e4ad86b98db128fbe3e2f46e290f021f0
prikol769 <prikol769@gmail.com>
2024-02-05 13:03:18 -0600

    added Navbar to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 export default function Page() {
   return (
-    <div>
-      <Live />
+    <div className="h-screen overflow-hidden">
+      <Navbar />
+
+      <section className="flex h-full flex-row">
+        <Live />
+      </section>
     </div>
   );
 }
